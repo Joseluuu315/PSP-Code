517baff7677babbf98cb852846b7b7ce9f34d7c9
José Luis Fuentes Parra <josefnts3115@gmail.com>
2025-11-13 18:04:53 +0100

    refactor -> package refactor

---
+++ src/com/joseluu/psp/Main.java
@@ -0,0 +1,19 @@
+package com.joseluu.psp;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+
+    /*
+    com.joseluu.psp.adivinar.Main to execute activity 8 tanda 1
+     */
+//    public static void main(String[] args) {
+//        NumeroOculto numeroOculto = new NumeroOculto();
+//
+//        for (int i = 1; i<=10; i++){
+//            HiloAdivinador h = new HiloAdivinador(numeroOculto, "hilo-" + i);
+//            h.start();
+//        }
+//    }
+}
